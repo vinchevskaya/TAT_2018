b77591a4e6576f017fc7ac9bb8fc196f6106ae60
vinchevskaya <36959546+vinchevskaya@users.noreply.github.com>
2018-03-03 12:31:09 +0300

    Add files via upload

---
+++ Beginning.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_1
+{
+
+    class Beginning
+    {
+        /// <summary>
+        /// This class deduced the number of characters from the longest repetition
+        /// </summary>
+        static void Main(string[] symbols)
+        {
+            if (symbols.Length == 1)
+            {
+                Workwithstring String = new Workwithstring(symbols[0]);
+                Console.WriteLine(String.maxRepetition());
+            }
+            else
+            {
+                Console.WriteLine("wrong view");
+            }
+        }
+
+
+    }
+}
+
+
+
---
+++ Workwithstring.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_1
+{
+    /// <summary>
+    /// This class is for working with a string
+    /// </summary>
+    class Workwithstring
+    {
+        private string currentString;
+
+        /// <summary>
+        /// This method finds repetitions of the symbols in the string
+        /// </summary>
+        /// <returns> The maximum number of symbol repetitions per line </returns>
+        public int maxRepetition()
+        {
+            int maxLength = 0;
+            int count = 0;
+            char firstSymbol = currentString[0];
+            for (int i = 0; i < currentString.Length; i++)
+            {
+                if (firstSymbol == currentString[i])
+                {
+                    count++;
+                    if (maxLength < count)
+                    {
+                        maxLength = count;
+                    }
+                }
+                else
+                {
+                    firstSymbol = currentString[i];
+                    count = 0;
+                }
+            }
+            return maxLength;
+        }
+
+        public Workwithstring(string ourString)
+        {
+            currentString = ourString;
+        }
+    }
+}
+
+
+
