f1d81c974c7db3ab2510f684467f7aaa5018c681
vinchevskaya <36959546+vinchevskaya@users.noreply.github.com>
2018-03-04 15:05:28 +0300

    Add files via upload

---
+++ DEV-2/FindingOfEvenIndices.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_2
+{
+    /// <summary>
+    /// This class looks for all even indexes in a string.
+    /// </summary>
+    class FindingOfEvenIndices
+    {
+        private string entryString;
+
+        public FindingOfEvenIndices(string value)
+        {
+            entryString = value;
+        }
+
+        /// <summary>
+        /// This method removes all odd indexes and displays even.
+        /// </summary>
+        public void FindSequenceEvenIndex()
+        {
+            StringBuilder sb = new StringBuilder(entryString);
+
+            for (int i = 1; i < sb.Length; i++)
+            {
+                sb.Remove(i, 1);
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
---
+++ DEV-2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_2
+{
+    /// <summary>
+    /// The entry point to the program.
+    /// </summary>
+    class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            FindingOfEvenIndices inputString = new FindingOfEvenIndices(Console.ReadLine());
+            inputString.FindSequenceEvenIndex();
+        }  
+    }  
+}
