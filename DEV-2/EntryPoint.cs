a415c7d9d4e9627758c91570dc9bd01b4ef42254
vinchevskaya <36959546+vinchevskaya@users.noreply.github.com>
2018-03-05 07:53:58 +0300

    Update EntryPoint.cs

--- DEV-2/EntryPoint.cs
+++ DEV-2/EntryPoint.cs
@@ -11,7 +11,6 @@ namespace DEV_2
         static void Main(string[] args)
         {
             OnlyEvenElementsOfTheString entryString = new OnlyEvenElementsOfTheString(Console.ReadLine());
-            entryString.FindSequenceEvenIndex();
             Console.WriteLine(entryString.FindSequenceEvenIndex());
         }  
     }  
