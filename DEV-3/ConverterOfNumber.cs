43173e32d031efe745b525d7767cd096f1579642
vinchevskaya <36959546+vinchevskaya@users.noreply.github.com>
2018-03-07 00:42:09 +0300

    Add files via upload

---
+++ DEV-3/ConverterOfNumber.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_3
+{
+    /// <summary>
+    /// This class converts a decimal number to another number system from 2 to 20.
+    /// </summary>
+    class ConverterOfNumber
+    {
+        int ConvertNumber;
+        int radix;
+
+        public ConverterOfNumber(int arg1, int arg2)
+        {
+            ConvertNumber = arg1;
+            radix = arg2;
+        }
+
+        /// <summary>
+        /// This method converts a decimal number to a new number system.
+        /// </summary>
+        /// <returns> Return a converted number in another number system. </returns>
+        public string СonvertDeciminalNumber()
+        {
+            string result = "";
+            int rest;
+            int buffer = ConvertNumber;
+            while (buffer > 0)
+            {
+                rest = buffer % radix;
+                if (rest < 9)
+                {
+                    result += rest;
+                }
+                else
+                {
+                    char newRadix = (char)(65 + rest - 10);
+                    result += newRadix;
+                }
+                buffer = buffer / radix;
+            }
+            return (Сonverse(result));
+        }
+
+        /// <summary>
+        /// This method returns a value.
+        /// </summary>
+        /// <param name="CounterString">return string, taken from another method</param>
+        /// <returns>returns the reverse-converted string</returns>
+        private string Сonverse(string CounterString)
+        {
+            char[] ConvertString = CounterString.ToCharArray();
+            Array.Reverse(ConvertString);
+            return new string(ConvertString);
+        }
+    }
+}
+
---
+++ DEV-3/EntryPoint.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+
+namespace DEV_3
+{
+    class EntryPoint
+    {
+        static void Main(string[] arg)
+        {
+            if (arg.Length != 2)
+            {
+                Console.Write("Wrong number of arguments");
+                Console.ReadLine();
+            }
+            else
+            {
+                if (Int32.Parse(arg[1]) > 2 || Int32.Parse(arg[1]) < 20)
+                {
+                    ConverterOfNumber conventer = new ConverterOfNumber(Int32.Parse(arg[0]), Int32.Parse(arg[1]));
+                    Console.WriteLine($"Converted number: {conventer.СonvertDeciminalNumber()}");
+                    Console.ReadLine();
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
