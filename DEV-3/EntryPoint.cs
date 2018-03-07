fabfb58ce22250c8325f4c55e20fdc2a668ea32b
vinchevskaya <36959546+vinchevskaya@users.noreply.github.com>
2018-03-07 15:56:12 +0300

    Update EntryPoint.cs

--- DEV-3/EntryPoint.cs
+++ DEV-3/EntryPoint.cs
@@ -1,23 +1,19 @@
-﻿using System;
+using System;
 using System.Text;
 
 namespace DEV_3
 {
+    /// <summary>
+    /// It is entry point of the program and it is print a number in the selected number system.
+    /// </summary>
     class EntryPoint
     {
         static void Main(string[] arg)
         {
-            if (arg.Length != 2)
+            if (Int32.Parse(arg[1]) > 2 || Int32.Parse(arg[1]) < 20)
             {
-                Console.Write("Wrong number of arguments");
-            }
-            else
-            {
-                if (Int32.Parse(arg[1]) > 2 || Int32.Parse(arg[1]) < 20)
-                {
-                    ConverterOfNumber conventer = new ConverterOfNumber(Int32.Parse(arg[0]), Int32.Parse(arg[1]));
-                    Console.WriteLine($"Converted number: {conventer.СonvertDeciminalNumber()}");
-                }
+                ConverterOfNumber conventer = new ConverterOfNumber(Int32.Parse(arg[0]), Int32.Parse(arg[1]));
+                Console.WriteLine($"{conventer.СonvertDeciminalNumber()}");
             }
         }
     }
