14cfd6a449e73e9fb121a590cb9e54682d558d31
vinchevskaya <36959546+vinchevskaya@users.noreply.github.com>
2018-03-07 20:02:01 +0300

    Update EntryPoint.cs

--- DEV-3/EntryPoint.cs
+++ DEV-3/EntryPoint.cs
@@ -10,10 +10,32 @@ namespace DEV_3
     {
         static void Main(string[] arg)
         {
-            if (Int32.Parse(arg[1]) > 2 || Int32.Parse(arg[1]) < 20)
+            try
             {
+                int decimalNumber = Int32.Parse(arg[0]);
+                if (decimalNumber < 0)
+                {
+                    throw new Exception("Can't convert negative numbers");
+                }
+                int newRadix = Int32.Parse(arg[1]);
+                if (newRadix < 2 || newRadix > 20)
+                {
+                    throw new Exception("wrong Radix");
+                }
                 ConverterOfNumber conventer = new ConverterOfNumber(Int32.Parse(arg[0]), Int32.Parse(arg[1]));
                 Console.WriteLine($"{conventer.СonvertDeciminalNumber()}");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("ERROR: " + ex.Message);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine("ERROR: Wrong number of arguments");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error:" + ex.Message);
             }
         }
     }
