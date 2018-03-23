fba21aff3d384dc0af41b46cd6cda37d270c777d
vinchevskaya <36959546+vinchevskaya@users.noreply.github.com>
2018-03-23 17:47:10 +0300

    Add files via upload

---
+++ Task_6.Test/ProductBuilderTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Task_6;
+
+namespace Task_6.Test
+{
+    [TestFixture]
+    public class ProductBuilderTest
+    {
+        [Test]
+        public void SetTypeNullTest()
+        {
+            var product = new ProductBuilder();
+            Assert.Throws<ArgumentException>(() => product.SetType(string.Empty));
+        }
+        [Test]
+        public void SetNameNullTest()
+        {
+            var product = new ProductBuilder();
+            Assert.Throws<ArgumentException>(() => product.SetName(string.Empty));
+        }
+    }
+}
---
+++ Task_6/EntryClass.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_6
+{
+    class EntryClass
+    {
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo continueInput = new ConsoleKeyInfo();
+
+            List<Shop> productList = new List<Shop>();
+            try
+            {
+                do
+                {
+                    var product = new ProductBuilder();
+
+                    Console.WriteLine("\nInput type of product:");
+                    product.SetType(Console.ReadLine());
+
+                    Console.WriteLine("\nInput the name of product:");
+                    product.SetName(Console.ReadLine());
+
+                    Console.WriteLine("\nInput the quantity of product");
+                    product.SetQuantity(Console.ReadLine());
+
+                    Console.WriteLine("\nInput the cost of one product");
+                    product.SetCostPerUnit(Console.ReadLine());
+
+                    productList.Add(product.Build());
+
+                    continueInputProducts:
+
+                    Console.WriteLine("\nDo you want to continue to input products?");
+                    Console.WriteLine("Yes [LeftArrow] | No [RightArrow]");
+                    continueInput = Console.ReadKey();
+
+                    if (continueInput.Key != ConsoleKey.RightArrow && continueInput.Key != ConsoleKey.LeftArrow) goto continueInputProducts;
+                }
+                while (continueInput.Key == ConsoleKey.LeftArrow);
+
+                string number;
+
+                do
+                {
+                    chooseCommand:
+
+                    Console.WriteLine("\nChoose command:" +
+                    "\n1) count types\n" +
+                    "2) count all\n" +
+                    "3) average price (all types)\n" +
+                    "4) average price (type)\n" +
+                    "5) exit\n" +
+                    "Input number: ");
+
+                    number = Console.ReadLine();
+
+                    Storage storage = new Storage();
+
+                    switch (number)
+                    {
+                        case "1":
+                            storage.SetCommand(new CountTypes());
+                            storage.WorkCommand(productList);
+                            break;
+                        case "2":
+                            storage.SetCommand(new CountAll());
+                            storage.WorkCommand(productList);
+                            break;
+                        case "3":
+                            storage.SetCommand(new AveragePrice());
+                            storage.WorkCommand(productList);
+                            break;
+                        case "4":
+                            Console.WriteLine("Input type: ");
+                            string type = Console.ReadLine();
+                            storage.SetCommand(new AveragePriceType(type));
+                            storage.WorkCommand(productList);
+                            break;
+                        case "5":
+                            return;
+                        default:
+                            goto chooseCommand;
+                    }
+                }
+                while (true);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
