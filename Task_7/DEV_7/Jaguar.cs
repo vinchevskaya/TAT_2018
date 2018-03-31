73c4abe30009fe68a8b3fd8070399a18068d1abe
vinchevskaya <36959546+vinchevskaya@users.noreply.github.com>
2018-03-31 13:14:47 +0300

    Add files via upload

---
+++ Task_7/DEV_7/Jaguar.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace DEV_7
+{
+    public class Jaguar : ICars 
+    {
+        public string Mark { get; set; }
+        public string Model { get; set; }
+        public string BodyType { get; set; }
+        public string TransmissionType { get; set; }
+        public string EngineType { get; set; }
+        public string Amount { get; set; }
+        public string Power { get; set; }
+        public string ClimateControl { get; set; }
+        public string InteriorType { get; set; }
+
+        public Jaguar(string mark, string model, string bodyType, string transmissionType, string engineType, string amount,
+          string power, string climateControl, string interiorType)
+        {
+            Mark = mark;
+            Model = model;
+            BodyType = bodyType;
+            TransmissionType = transmissionType;
+            EngineType = engineType;
+            Amount = amount;
+            Power = power;
+            ClimateControl = climateControl;
+            InteriorType = interiorType;
+        }
+    }
+}
---
+++ Task_7/DEV_7/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_7
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Select the desired characteristics: ");
+            Console.WriteLine("1: Mark | (Lada, Jaguar, Lexus)");
+            Console.WriteLine("2: Model");
+            Console.WriteLine("3: BodyType | (Sedan, SUV)");
+            Console.WriteLine("4: Transmission | (Automatic, Manual)");
+            Console.WriteLine("5: EngineType | (Gasoline, Diesel, Electric)");
+            Console.WriteLine("6: Amount");
+            Console.WriteLine("7: Power");
+            Console.WriteLine("8: ClimateManagement | (ClimateControle, Conditioner, Null)");
+            Console.WriteLine("9: InteriorType | (Leather, Fabric, Combined)");
+
+            string mark = Console.ReadLine();
+            string model = Console.ReadLine();
+            string bodyType = Console.ReadLine();
+            string transmissionType = Console.ReadLine();
+            string engineType = Console.ReadLine();
+            string amount = Console.ReadLine();
+            string power = Console.ReadLine();
+            string climateManagement = Console.ReadLine();
+            string interiorType = Console.ReadLine();
+
+            //ICars cars = new Jaguar("XE", "sedan", "revel", "", "", "", "", "leather");
+            string stock = "Catalog.json";
+            ICreator creator = new JaguarCreator("Jaguar", "XE", "sedan", "automatic", "", "", "", "", "leather");
+            creator.CarsCreator();
+            JSONreader Reader = new JSONreader(stock);
+            Reader.ReadToObjectJson();
+        }
+    }
+}
