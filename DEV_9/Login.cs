aba78b44b0bf25b1f3a9b68e336acafb887d5c98
vinchevskaya <36959546+vinchevskaya@users.noreply.github.com>
2018-05-09 14:00:03 +0300

    Add files via upload

---
+++ DEV_9/Login.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace DEV_9
+{
+    class Login
+    {
+        /// <summary>
+        /// This class comes to the page VK,
+        /// clicks the Messages button, reads the first message
+        /// and closes the browser.
+        /// </summary>
+        IWebDriver driver = new ChromeDriver();              
+        public void News(string login, string password)
+        {
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            
+            driver.Url = "https://vk.com/";
+            if (driver.Url != "https://vk.com/")
+            {
+                throw new Exception("Error");
+            }
+            driver.FindElement(By.XPath("//*[@id='index_email']")).SendKeys(login);
+            driver.FindElement(By.XPath("//*[@id='index_pass']")).SendKeys(password + Keys.Enter);
+
+            IWebElement element = driver.FindElement(By.XPath("//li[@id='l_msg']"));
+            element.Click();
+
+            IWebElement firstElement = driver.FindElement(By.XPath("//*[@id='im_dialogs']/li[1]"));
+            element.Click();
+            
+            driver.Quit();
+        }
+    }
+}
---
+++ DEV_9/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DEV_9
+{
+    /// <summary>
+    /// This is Entry Point to the program.
+    /// You need to enter login and password from the page VK.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter login: ");
+            string login = Console.ReadLine();
+            Console.WriteLine("Enter password: ");
+            string password = Console.ReadLine();
+
+            Login loginForInput = new Login();
+            loginForInput.News(login, password);
+        }
+    }
+}
